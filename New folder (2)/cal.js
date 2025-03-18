eb9acae67b23fbcfb0a980e1fba43520587a7ffc
Souvik Bhunia <bhunsouvik7@gmail.com>
2025-03-18 11:00:15 +0530

    add files

---
+++ New folder (2)/cal.js
@@ -0,0 +1,19 @@
+
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (e) {
+        alert('Invalid Expression');
+    }
+}
